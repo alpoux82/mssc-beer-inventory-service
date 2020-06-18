2a7a63f64a98b8994909e0bc7594c0dae638309f
Alberto <alberto.poyatos.uxo@gmail.com>
2020-06-18 19:57:05 +0200

    Add AllocationService

---
+++ src/main/java/guru/springframework/msscbeerinventoryservice/services/AllocationServiceImpl.java
@@ -0,0 +1,65 @@
+package guru.springframework.msscbeerinventoryservice.services;
+
+import guru.sfg.brewery.model.BeerOrderDto;
+import guru.sfg.brewery.model.BeerOrderLineDto;
+import guru.springframework.msscbeerinventoryservice.domain.BeerInventory;
+import guru.springframework.msscbeerinventoryservice.repositories.BeerInventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RequiredArgsConstructor
+@Service
+public class AllocationServiceImpl implements AllocationService {
+
+    private final BeerInventoryRepository beerInventoryRepository;
+
+    @Override
+    public Boolean allocateOrder(BeerOrderDto beerOrderDto) {
+        AtomicInteger totalOrdered = new AtomicInteger();
+        AtomicInteger totalAllocated = new AtomicInteger();
+
+        beerOrderDto.getBeerOrderLines().forEach(beerOrderLineDto -> {
+            if (needToAllocate(beerOrderLineDto)) {
+                allocateBeerOrderLineDto(beerOrderLineDto);
+            }
+            totalOrdered.set(totalOrdered.get() + beerOrderLineDto.getOrderQuantity());
+            totalAllocated.set(totalAllocated.get() + (beerOrderLineDto.getQuantityAllocated() != null ? beerOrderLineDto.getQuantityAllocated() : 0));
+
+        });
+
+        return totalOrdered.get() == totalAllocated.get();
+    }
+
+    private boolean needToAllocate(BeerOrderLineDto beerOrderLineDto) {
+        return ((beerOrderLineDto.getOrderQuantity() != null ? beerOrderLineDto.getOrderQuantity() : 0 ) -
+                (beerOrderLineDto.getQuantityAllocated() != null ? beerOrderLineDto.getQuantityAllocated() : 0)) > 0;
+    }
+
+    private void allocateBeerOrderLineDto(BeerOrderLineDto beerOrderLineDto) {
+        List<BeerInventory> beerInventoryList = beerInventoryRepository.findAllByUpc(beerOrderLineDto.getUpc());
+        beerInventoryList.forEach(beerInventory -> {
+            int inventory = beerInventory.getQuantityOnHand() == null ? 0 : beerInventory.getQuantityOnHand();
+            int orderQty = beerOrderLineDto.getOrderQuantity() == null ? 0 : beerOrderLineDto.getOrderQuantity();
+            int allocatedQty = beerOrderLineDto.getQuantityAllocated() == null ? 0 : beerOrderLineDto.getQuantityAllocated();
+            int qtyToAllocate = orderQty - allocatedQty;
+
+            if (inventory >= qtyToAllocate) {
+                inventory = inventory - qtyToAllocate;
+                beerOrderLineDto.setQuantityAllocated(orderQty);
+                beerInventory.setQuantityOnHand(inventory);
+
+                beerInventoryRepository.save(beerInventory);
+            }
+            else if (inventory > 0) {
+                beerOrderLineDto.setQuantityAllocated(allocatedQty + inventory);
+                beerInventory.setQuantityOnHand(0);
+            }
+
+            if (beerInventory.getQuantityOnHand() == 0)
+                beerInventoryRepository.delete(beerInventory);
+        });
+    }
+}
